9bee032141d2cd0ae9fbdf03d00ae7e67911f5e5
Abdulsalam <abdulmuhd857@gmail.com>
2024-01-18 11:53:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,22 @@
-// import Navigation from "./Navigation/Nav";
-// import Recommended from "./Recommended/Recommended";
-import Categories from './Categories';
-import Menu from './Menu'
-// import Sidebar from "./Sidebar/Sidebar";
-// import Products from "./products/Products";
-import items from './data';
-import { useState } from "react";
-
-const allCategories =['all',... new Set(items.map((item) => item.category))]
-console.log(allCategories);
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Featured from './components/Featured'
+import SignUp from './components/SignUp'
+import Footer from './components/Footer'
 
 
-function App() {
-
-  const[menuItems, setMenuItems] = useState(items);
-  const[categories, setCategories] = useState(allCategories);
-  
-  const filterItems = (category) => {
-    if(category ==='all') {
-      setMenuItems(items)
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category)
-    setMenuItems(newItems)
-  }
-
-
+const App = () => {
   return (
-  <main>
-    <section className="menu section">
-      <div className="title">
-        <h2>Our Menu</h2>
-        <div className="underline"></div>
-      </div>
-      <Categories categories={categories} filterItems={filterItems} />
-      <Menu items={menuItems} />
-    </section>
-  </main>
-   
+    <div>
+        <Navbar />
+        <Hero />
+        <Featured />
+        <SignUp />
+        <Footer />
+
+    </div>
   )
 }
- 
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import React from 'react'
+import './Navbar.css'
+import react from 'react'
+import { icons } from 'antd/es/image/PreviewGroup'
+
+const Navbar = () => {
+
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+  return (
+    <div className='header'>
+        <div className="container">
+            <h1>De <span className='primary'>Fi</span></h1>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">Feaured</a>
+                </li>
+                <li>
+                    <a href="/">Earn</a>
+                </li>
+                <li>
+                    <a href="/">Contact</a>
+                </li>
+            </ul>
+            <div className="btn-group">
+                <button className="btn">
+                    Connect Wallet
+                </button>
+            </div>
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (<FaTimes size={20} style={{color:'#333'}} />) : <FaBars size={20} style={{color:'#333'}} /> }
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar

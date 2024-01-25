0dcf9cc930793225845419fab7540cb39727c1b2
Abdulsalam <abdulmuhd857@gmail.com>
2024-01-25 21:21:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Chat from './components/Chat';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HeadlineCard from './components/HeadlineCard'
+import Food from './components/Food'
+import Category from './components/Category'
 
-import { auth } from './firebase';
-import { useAuthState } from 'react-firebase-hooks/auth';
-
-const style = {
-  appContainer: `max-w-[728px] mx-auto text-center`,
-  sectionContainer: `flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative`,
-};
-
-function App() {
-  const [user] = useAuthState(auth);
-  //  console.log(user)
+const App = () => {
   return (
-    <div className={style.appContainer}>
-      <section className='{style.sectionContainer}'>
-        {/* Navbar */}
-        <Navbar />
-        {user ? <Chat /> : null}
-        {/* <Chat /> */}
-      </section>
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCard />
+      <Food />
+      <Category />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,70 @@
-import React from 'react';
-// import LogOut from './LogOut'
-import {auth} from '../firebase'
-import {useAuthState} from 'react-firebase-hooks/auth'
-import Signin from './Signin';
-import LogOut from './LogOut';
-const style = {
-    nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
-    heading: `text-white text-3xl`
-}
+import React, {useState} from 'react'
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react-icons/ai' 
+import { BsFillCartFill, BsFillSaveFill } from 'react-icons/bs'
+import { TbTruckDelivery } from 'react-icons/tb'
+import { MdFavorite, MdHelp } from 'react-icons/md'
+import { FaWallet, FaUserFriends } from 'react-icons/fa'
+
+
 
 const Navbar = () => {
-    const [user] = useAuthState(auth)
-    console.log(user)
+  const [nav, setNav] = useState(false)
   return (
-    <div className={style.nav}>
-      <h1 className={style.heading}>Chat App</h1>
-      {user ? <LogOut /> : <Signin />}
-      {/* <Signin />
-      <LogOut /> */}
+    <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
+        {/* left side */}
+        <div className='flex items-center'>
+            <div onClick={() => setNav(!nav)}>
+                <AiOutlineMenu size={30} />
+            </div>
+            <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
+                Best <span className='font-bold'>Eats</span>
+            </h1>
+            <div className='hidden lg:flex items-center bg-gray-200 rounded-full
+            p-1 text-[14px]'>
+                <p className='bg-black text-white rounded-full'>Delivery</p>
+                <p className='p-2'>Pickup</p>
+            </div>
+        </div>
+        {/* srearch input */}
+
+        <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
+            <AiOutlineSearch size={25} />
+            <input className='bg-transparent p-2 w-full focus:outline-none' type="text" placeholder='Search foods' />
+        </div>
+         {/* Cart button */}
+      <button className='bg-black text-white hidden md:flex items-center py-2 rounded-full'>
+        <BsFillCartFill size={20} className='mr-2' /> Cart
+      </button>
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      {nav ? <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div> : ''}
+      
+      
+      {/* Side drawer menu */}
+      <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300 ' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300 ' }>
+        <AiOutlineClose
+            onClick={()=> setNav(!nav)}
+          size={30}
+          className='absolute right-4 top-4 cursor-pointer'
+        />
+        <h2 className='text-2xl p-4'>
+          Best <span className='font-bold'>Eats</span>
+        </h2>
+        <nav>
+            <ul className='flex flex-col p-4 text-gray-800'>
+                <li className='text-xl py-4 flex'><TbTruckDelivery size={25} className='mr-4' /> Orders</li>
+                <li className='text-xl py-4 flex'><MdFavorite size={25} className='mr-4' /> Favorites</li>
+                <li className='text-xl py-4 flex'><FaWallet size={25} className='mr-4' /> Wallet</li>
+                <li className='text-xl py-4 flex'><MdHelp size={25} className='mr-4' /> Help</li>
+                <li className='text-xl py-4 flex'><AiFillTag size={25} className='mr-4' /> Promotions</li>
+                <li className='text-xl py-4 flex'><BsFillSaveFill size={25} className='mr-4' /> Best Ones</li>
+                <li className='text-xl py-4 flex'><FaUserFriends size={25} className='mr-4' /> Invite Friends</li>
+            </ul>
+        </nav>
+      </div>
 
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
